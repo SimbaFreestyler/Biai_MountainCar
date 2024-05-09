8611e91c73cb735ce3501e200408f3eb02e112c3
Jakub Golder <jg300716@student.polsl.pl>
2024-05-09 14:41:12 +0200

    .

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import gymnasium as gym
+gym.make('MountainCar-v0')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
